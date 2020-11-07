a73aa97b2f89b2a20a7fe0631d903a556f2749f2
jomapormentilla <jomapormentilla@gmail.com>
2020-11-07 13:38:14 -0500

    CLI update - Ability to view Friends and Spells from CLI

--- lib/concerns/commands.rb
+++ lib/concerns/commands.rb
@@ -24,10 +24,10 @@ module Commands
             input_name
 
             puts "      Hello, #{ @name }! Your journey begins with The Sorting Hat."
-            sleep(1)
+            sleep(2)
             puts "      Let's see which House you belong to..."
             puts ""
-            sleep(1)
+            sleep(2)
             puts "      ... #{ @house.upcase }!\n"
         end
 
@@ -38,7 +38,7 @@ module Commands
             
             # Display available commands
             @commands.each.with_index do |command, index|
-                puts "#{ index + 1 }. #{ command }"
+                puts "      #{ index + 1 }. #{ command }"
             end
     
             # Collect user input
@@ -53,12 +53,14 @@ module Commands
             when 3
                 puts "      List only Students in your House"
             when 4
-                puts "      List all available Spells"
+                puts "      Here are all the spells you currently know:"
+                view_spells
             when 5
                 puts "      Who would you like to find?"
                 search = find_wizard
             when 6
-                puts "      Your friends are:"
+                puts "      Here is a list of your friends:"
+                view_friends
             when 7
                 puts "      Your enemies are:"
             when 8
@@ -71,6 +73,7 @@ module Commands
                 puts "Invalid Selection."
                 level_one_commands   # Ask for input again
             end
+            sleep(2)
             level_one_commands
         end
 
@@ -79,10 +82,10 @@ module Commands
             result = Wizard.find_by_name( input )
 
             if result == nil
-                puts "      Sorry, it seems #{ input } does not go to Hogwarts! Try searching a different name:"
+                puts "      => Sorry, it seems #{ input } does not go to Hogwarts! Try searching a different name:"
                 find_wizard
             else
-                puts "      #{ result.name } is a #{ result.role } from House #{ result.house.name }."
+                puts "      => #{ result.name } is a #{ result.role } from House #{ result.house.name }."
                 friend_wizard( result )
             end
         end
@@ -92,11 +95,29 @@ module Commands
             
             input = gets.strip
             if input == "Y"
-                puts "      Great! You are now friends with #{ wizard.name }!"
+                puts "      => Great! You are now friends with #{ wizard.name }!"
                 self.info.add_friend( wizard )
             elsif input == "N"
-                puts "      Uh oh, you have upset #{ wizard.name }! You are now enemies."
+                puts "      => Uh oh, you have upset #{ wizard.name }! You are now enemies."
                 self.info.add_enemy( wizard )
+            end
+        end
+
+        def view_friends
+            my_friends = self.info.friends
+            if my_friends == []
+                puts "      => You don't have any friends."
+            else
+                my_friends.each.with_index(1){ |friend, index| puts "       #{ index }. #{ friend.name }" }
+            end
+        end
+
+        def view_spells
+            my_spells = self.info.list_spells
+            if my_spells == []
+                puts "      => You don't know any spells."
+            else
+                my_spells.each.with_index(1){ |spell, index| puts "#{ index }. #{ spell }" }
             end
         end
         
@@ -107,7 +128,7 @@ module Commands
                 @name = name
                 add_student
             else
-                puts "      I can assure you that you are NOT invisible! You will learn that spell in your 2nd week :) \nPlease enter a valid name:"
+                puts "      => I can assure you that you are NOT invisible! You will learn that spell in your 2nd week :) \nPlease enter a valid name:"
                 input_name
             end
         end
@@ -121,12 +142,12 @@ module Commands
         end
     
         def welcome_banner
-            puts "    *     *       *     *     "
-            puts " *            *              *"
-            puts "    Welcome to Hogwarts!      "
-            puts "           *          *       "
-            puts "      *        *           *  "
-            puts "                              "
+            puts "    *             *     *       "
+            puts " *        *                  *  "
+            puts "      Welcome to Hogwarts!      "
+            puts "  *                   *         "
+            puts "      *        *           *    "
+            puts "                                "
         end
     end
 end
